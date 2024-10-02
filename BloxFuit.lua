302e422f22b035c7aa1ea0eb178d1e734e6603c9
NotYouPasToi <zizinatanael3@gmail.com>
2024-10-02 14:04:27 +0200

    Update BloxFuit.lua

--- BloxFuit.lua
+++ BloxFuit.lua
@@ -33,9 +33,9 @@ local Slider = PlayerTab:CreateSlider({
    Range = {10, 100},
    Increment = 1,
    Suffix = "Speed",
-   CurrentValue = 10,
+   CurrentValue = 1,
    Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
    Callback = function(Value)
-   
+   game.Players.LocalPlayer.Character:SetAttribute("SpeedMultipier, Value")
    end,
 })
